0babb852b4a878c5f5473c27a67776edb95a6037
Street Lethal <street.lethal.57@gmail.com>
2020-05-19 00:04:27 +0900

    fix unrar

--- scripts/internal/unzip.sh
+++ scripts/internal/unzip.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-unrar e $@ zipped/*.rar unzipped/
+for file in `ls zipped/ | grep .rar$`; do
+    mkdir unzipped/$file
+    unrar e $@ zipped/$file unzipped/$file
+done
 chown 1000:1000 -R unzipped
